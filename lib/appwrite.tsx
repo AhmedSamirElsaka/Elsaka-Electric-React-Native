538c6c3d0401c32a2abdf7df0b0099d5ce105d33
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-08-30 17:38:52 +0300

    initialize appwrite and authentication form field

---
+++ lib/appwrite.tsx
@@ -0,0 +1,128 @@
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  ID,
+  ImageGravity,
+  Query,
+  Storage,
+} from "react-native-appwrite";
+
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.ahmedsamir.elsakaelectric",
+  projectId: "66d1d66d002baaf15d33",
+  databaseId: "66d1d90e003cb70104ce",
+  userCollectionId: "66d1d931003d64508125",
+};
+
+const client = new Client();
+
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+const account = new Account(client);
+const storage = new Storage(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+// Register user
+export async function createUser(
+  email: string,
+  password: string,
+  username: string
+) {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw Error;
+
+    // console.log(newAccount, "newAccount");
+    const avatarUrl = avatars.getInitials(username);
+
+    try {
+      const session = await account.createEmailPasswordSession(email, password);
+    } catch (error: any) {
+      throw new Error(error);
+    }
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        asccountId: newAccount.$id,
+        email: email,
+        username: username,
+        avatar: avatarUrl,
+      }
+    );
+
+    // console.log(newUser, "newUser");
+    return newUser;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+// Sign In
+export async function signIn(email: string, password: string) {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+
+    return session;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+// Get Account
+export async function getAccount() {
+  try {
+    const currentAccount = await account.get();
+
+    return currentAccount;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
+
+// Get Current User
+export async function getCurrentUser() {
+  try {
+    const currentAccount = await getAccount();
+    if (!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("asccountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    // console.log(currentUser, "currentUser");
+    return currentUser.documents[0];
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+// Sign Out
+export async function signOut() {
+  try {
+    const session = await account.deleteSession("current");
+
+    return session;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
