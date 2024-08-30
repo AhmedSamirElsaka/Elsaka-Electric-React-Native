b2e37cd855dc1d8d6194ded87476dc64b9b80fa1
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-08-30 08:22:11 +0300

    initialize the OnBoardingScreenItem

--- components/OnBoardingButton.tsx
+++ components/OnBoardingButton.tsx
@@ -1,14 +1,158 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  useWindowDimensions,
+} from "react-native";
 import React from "react";
+import Animated, {
+  AnimatedRef,
+  SharedValue,
+  interpolateColor,
+  useAnimatedStyle,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+import { useNavigation } from "@react-navigation/native";
+import { OnboardingData } from "../data/onboardingData";
+import { StackNavigationProp } from "@react-navigation/stack";
 
-const OnBoardingButton = () => {
+type Props = {
+  dataLength: number;
+  flatListIndex: SharedValue<number>;
+  flatListRef: AnimatedRef<FlatList<OnboardingData>>;
+  x: SharedValue<number>;
+};
+
+const OnBoardingButton = ({
+  flatListRef,
+  flatListIndex,
+  dataLength,
+  x,
+}: Props) => {
+  const { width: SCREEN_WIDTH } = useWindowDimensions();
+  const navigation = useNavigation();
+
+  const animatedBackgroundColor = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      x.value,
+      [0, SCREEN_WIDTH, 2 * SCREEN_WIDTH],
+      ["transparent", "transparent", "#F15937"]
+    );
+
+    return {
+      backgroundColor: backgroundColor,
+    };
+  });
+
+  const containerAnimationStyle = useAnimatedStyle(() => {
+    return {
+      width:
+        flatListIndex.value === dataLength - 1
+          ? withTiming(120)
+          : withTiming(60),
+
+      height:
+        flatListIndex.value === dataLength - 1
+          ? withTiming(40)
+          : withTiming(30),
+    };
+  });
+  const nextAnimationStyle = useAnimatedStyle(() => {
+    return {
+      width: 60,
+      height: 30,
+      opacity:
+        flatListIndex.value === dataLength - 1 ? withTiming(0) : withTiming(1),
+      transform: [
+        {
+          translateX:
+            flatListIndex.value === dataLength - 1
+              ? withTiming(180)
+              : withTiming(0),
+        },
+      ],
+    };
+  });
+
+  const getStartedTextAnimationStyle = useAnimatedStyle(() => {
+    return {
+      opacity:
+        flatListIndex.value === dataLength - 1 ? withTiming(1) : withTiming(0),
+      transform: [
+        {
+          translateX:
+            flatListIndex.value === dataLength - 1
+              ? withTiming(0)
+              : withTiming(-100),
+        },
+      ],
+    };
+  });
+
   return (
-    <View>
-      <Text>OnBoardingButton</Text>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        if (flatListIndex.value < dataLength - 1) {
+          flatListRef.current?.scrollToIndex({
+            index: flatListIndex.value + 1,
+          });
+        } else {
+          navigation.navigate("Home");
+        }
+      }}
+    >
+      <Animated.View
+        style={[
+          styles.container,
+          animatedBackgroundColor,
+          containerAnimationStyle,
+        ]}
+      >
+        <Animated.Text
+          style={[styles.textButton, getStartedTextAnimationStyle]}
+        >
+          Get Started
+        </Animated.Text>
+
+        <Animated.Text style={[styles.nextText, nextAnimationStyle]}>
+          Next
+        </Animated.Text>
+      </Animated.View>
+    </TouchableWithoutFeedback>
   );
 };
 
 export default OnBoardingButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    width: 120,
+    height: 30,
+    padding: 6,
+    borderRadius: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  nextText: {
+    fontSize: 20,
+    alignContent: "center",
+    justifyContent: "center",
+    textAlign: "right",
+  },
+  textButton: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    flex: 1,
+    position: "absolute",
+    textAlign: "right",
+  },
+  lastScreenText: {
+    backgroundColor: "#F15937",
+  },
+  allScreensText: {
+    backgroundColor: "transparent",
+  },
+});
---
+++ components/OnBoardingScreenItem.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const OnBoardingScreenItem = () => {
+  return (
+    <View>
+      <Text>OnBoardingScreenItem</Text>
+    </View>
+  );
+};
+
+export default OnBoardingScreenItem;
+
+const styles = StyleSheet.create({});
