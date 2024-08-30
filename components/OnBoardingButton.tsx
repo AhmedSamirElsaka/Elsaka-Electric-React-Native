53ab0b213c24a1741b7d8c895d6de5852f0bf149
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-08-30 08:21:11 +0300

    initializing the button of onboarding screen

---
+++ components/OnBoardingButton.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const OnBoardingButton = () => {
+  return (
+    <View>
+      <Text>OnBoardingButton</Text>
+    </View>
+  );
+};
+
+export default OnBoardingButton;
+
+const styles = StyleSheet.create({});
