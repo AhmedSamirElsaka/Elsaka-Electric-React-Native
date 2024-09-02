a4829503ecabd41fcb2552a5662f22ac5a430ed5
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-02 12:31:09 +0300

    finish the category list component

--- components/CategoriesList.tsx
+++ components/CategoriesList.tsx
@@ -1,10 +1,38 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { FlatList } from "react-native-gesture-handler";
 
-const CategoriesList = () => {
+const CategoriesList = ({
+  categories,
+  selectedItem,
+}: {
+  categories: string[];
+  selectedItem: string;
+}) => {
+  const renderItem = (item: string) => {
+    return (
+      <View className=" items-center space-y-1">
+        <Text
+          className={`text-gray-200 text-lg ${
+            item === selectedItem && "text-primary"
+          }`}
+        >
+          {item}
+        </Text>
+        {item === selectedItem && (
+          <View className="w-2 h-2 rounded-full bg-primary" />
+        )}
+      </View>
+    );
+  };
   return (
     <View>
-      <Text>CategoriesList</Text>
+      <FlatList
+        data={categories}
+        renderItem={({ item }) => renderItem(item)}
+        horizontal
+        ItemSeparatorComponent={() => <View className="w-4" />}
+      />
     </View>
   );
 };
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,21 +1,37 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
+import CategoriesList from "@/components/CategoriesList";
 
 const HomeScreen = () => {
   return (
-    <View className="flex-1  bg-mainBackground">
-      <Header title="Home" />
-      <Text className="text-white font-bold text-2xl pl-8 pr-12">
-        Find the best <Text className="text-primary">Electrical Product</Text>{" "}
-        for your home
-      </Text>
+    <ScrollView className="flex-1  bg-mainBackground">
+      <View className="flex-1  bg-mainBackground">
+        <Header title="Home" />
+        <Text className="text-white font-bold text-2xl pl-8 pr-12">
+          Find the best <Text className="text-primary">Electrical Product</Text>{" "}
+          for your home
+        </Text>
 
-      <View className="pt-8 px-6">
-        <SearchInput initialQuery={""} />
+        <View className="pt-8 px-6 mb-6">
+          <SearchInput initialQuery={""} />
+        </View>
+
+        <View className="px-6">
+          <CategoriesList
+            categories={[
+              "All",
+              "Cappuccino",
+              "Espresso",
+              "Americano",
+              "Macchiato",
+            ]}
+            selectedItem="All"
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
