e8bb3ece8537e83613dd575fc46b1b4a2baae45c
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-02 12:08:30 +0300

    initialize the categories list component

---
+++ components/CategoriesList.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CategoriesList = () => {
+  return (
+    <View>
+      <Text>CategoriesList</Text>
+    </View>
+  );
+};
+
+export default CategoriesList;
+
+const styles = StyleSheet.create({});
