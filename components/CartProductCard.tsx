51b386ec666fafdd3f59c32099730906e15260ac
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-09 12:00:07 +0300

    initialize the Cart Product Card Component

---
+++ components/CartProductCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CartProductCard = () => {
+  return (
+    <View>
+      <Text>CartProductCard</Text>
+    </View>
+  );
+};
+
+export default CartProductCard;
+
+const styles = StyleSheet.create({});
--- screens/CartScreen.tsx
+++ screens/CartScreen.tsx
@@ -1,9 +1,16 @@
-import { StatusBar, StyleSheet, Text, View } from "react-native";
+import { ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ProductDetails from "@/components/ProductDetails";
+import Header from "@/components/Header";
 
 const CartScreen = () => {
-  return <View className="flex-1  bg-mainBackground"></View>;
+  return (
+    <View className="flex-1  bg-mainBackground">
+      <ScrollView>
+        <Header title="Cart" />
+      </ScrollView>
+    </View>
+  );
 };
 
 export default CartScreen;
