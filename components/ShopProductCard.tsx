a0b05d5d8287ca5b035be474423192e53010ffaf
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-12 13:21:49 +0300

    initialize the Shop Product Cardcomponent

---
+++ components/ShopProductCard.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ShopProductCard = () => {
+  return (
+    <View>
+      <Text>ShopProductCard</Text>
+    </View>
+  );
+};
+
+export default ShopProductCard;
+
+const styles = StyleSheet.create({});
--- components/ShopProductList.tsx
+++ components/ShopProductList.tsx
@@ -1,10 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Product } from "@/types/types";
+import { FlatList } from "react-native";
 
-const ShopProductList = () => {
+const ShopProductList = ({ items }: { items: Product[] }) => {
   return (
     <View>
-      <Text>ShopProductList</Text>
+      <FlatList
+        data={items}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <ShopProductCard product={item} />}
+      />
     </View>
   );
 };
