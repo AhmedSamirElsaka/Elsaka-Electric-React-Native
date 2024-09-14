f3d68be4d3ca947eec94342c619821e07d43781b
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-14 20:22:05 +0300

    work on promo code section in cart screen

--- components/PromoCodeComponent.tsx
+++ components/PromoCodeComponent.tsx
@@ -20,17 +20,17 @@ const PromoCodeComponent = ({
       start={{ x: 0, y: 0 }}
       end={{ x: 1, y: 1 }}
       colors={["#252A32", "#0C0F14"]}
-      className="  flex-row justify-between px-4 bg-secondaryDarkGreyHex mx-4 rounded-xl mt-4"
+      className="  flex-row justify-between px-4 bg-secondaryDarkGreyHex mx-4 mr-8 rounded-xl mt-4"
     >
       <TextInput
-        className="bg-[#fff0] flex-1 -mr-6 rounded-xl px-4 text-white"
+        className="bg-[#fff0] flex-1 rounded-xl px-4 text-white"
         placeholder="Enter promo code"
         multiline={false}
         onFocus={onFocus}
         placeholderTextColor={"gray"}
       />
       <TouchableOpacity
-        className="bg-white rounded-full p-2"
+        className="bg-white rounded-full p-2 -mr-6"
         activeOpacity={0.7}
         onPress={onPress}
       >
--- screens/CartScreen.tsx
+++ screens/CartScreen.tsx
@@ -16,6 +16,7 @@ import ProductDetails from "./ProductDetails";
 import * as Icons from "react-native-heroicons/solid";
 import { LinearGradient } from "expo-linear-gradient";
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
+import PromoCodeComponent from "@/components/PromoCodeComponent";
 
 const CartScreen = () => {
   const [isPromoCodeBottomSheetShown, setIsPromoCodeBottomSheetShown] =
@@ -72,7 +73,13 @@ const CartScreen = () => {
           )}
         />
       </View>
-      <LinearGradient
+      <PromoCodeComponent
+        onFocus={() => {
+          bottomSheetRef.current?.expand();
+        }}
+        onPress={() => {}}
+      />
+      {/* <LinearGradient
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         colors={["#252A32", "#0C0F14"]}
@@ -93,7 +100,7 @@ const CartScreen = () => {
         >
           <Icons.ArrowRightIcon color="black" size={30} />
         </TouchableOpacity>
-      </LinearGradient>
+      </LinearGradient> */}
 
       <View className="flex-row  pt-4 justify-between mb-2">
         <View className="justify-center items-center pr-10  pl-6">
@@ -113,37 +120,26 @@ const CartScreen = () => {
       <BottomSheet
         ref={bottomSheetRef}
         // onChange={handleSheetChanges}
-        snapPoints={[380]}
+        snapPoints={[450]}
         index={0}
         backgroundStyle={{ backgroundColor: "#2B2D31" }}
         handleIndicatorStyle={{ backgroundColor: "white" }}
         handleStyle={{ borderRadius: 30 }}
         containerHeight={500}
-        containerStyle={{ backgroundColor: "#00000059" }}
+        // onClose={() => bottomSheetRef.current?.close()}
+        enablePanDownToClose
+        // containerStyle={{ backgroundColor: "#00000059" }}
       >
         <BottomSheetView className="bg-red flex-1 opacity-100">
-          <LinearGradient
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            colors={["#252A32", "#0C0F14"]}
-            className="  flex-row justify-between px-4 bg-secondaryDarkGreyHex mx-4 rounded-xl mt-4"
-          >
-            <TextInput
-              className="bg-[#fff0] flex-1 -mr-6 rounded-xl px-4 text-white"
-              placeholder="Enter promo code"
-              multiline={false}
-              onFocus={() => {
-                console.log("focus");
-              }}
-              placeholderTextColor={"gray"}
-            />
-            <TouchableOpacity
-              className="bg-white rounded-full p-2"
-              activeOpacity={0.7}
-            >
-              <Icons.ArrowRightIcon color="black" size={30} />
-            </TouchableOpacity>
-          </LinearGradient>
+          <PromoCodeComponent
+            onFocus={() => {}}
+            onPress={() => {
+              bottomSheetRef.current?.close();
+            }}
+          />
+          <Text className="text-white text-xl font-semibold px-6 pt-6">
+            Your Promo Codes
+          </Text>
         </BottomSheetView>
       </BottomSheet>
     </View>
