9aa8fb629ab0bc28435c3b0538981dc17fecb740
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-14 16:26:30 +0300

    initialize the PromoCodeComponent

---
+++ components/PromoCodeComponent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const PromoCodeComponent = () => {
+  return (
+    <View>
+      <Text>PromoCodeComponent</Text>
+    </View>
+  );
+};
+
+export default PromoCodeComponent;
+
+const styles = StyleSheet.create({});
--- screens/CartScreen.tsx
+++ screens/CartScreen.tsx
@@ -8,17 +8,20 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "@/components/Header";
 import CustomButton from "@/components/CustomButton";
 import CartProductCard from "@/components/CartProductCard";
 import ProductDetails from "./ProductDetails";
 import * as Icons from "react-native-heroicons/solid";
 import { LinearGradient } from "expo-linear-gradient";
+import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet";
 
 const CartScreen = () => {
   const [isPromoCodeBottomSheetShown, setIsPromoCodeBottomSheetShown] =
     useState(false);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
   return (
     // <View className="flex-1 bg-mainBackground">
     //   <ProductDetails
@@ -107,6 +110,42 @@ const CartScreen = () => {
           containerStyles="bg-primary flex-1 mr-6"
         />
       </View>
+      <BottomSheet
+        ref={bottomSheetRef}
+        // onChange={handleSheetChanges}
+        snapPoints={[380]}
+        index={0}
+        backgroundStyle={{ backgroundColor: "#2B2D31" }}
+        handleIndicatorStyle={{ backgroundColor: "white" }}
+        handleStyle={{ borderRadius: 30 }}
+        containerHeight={500}
+        containerStyle={{ backgroundColor: "#00000059" }}
+      >
+        <BottomSheetView className="bg-red flex-1 opacity-100">
+          <LinearGradient
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            colors={["#252A32", "#0C0F14"]}
+            className="  flex-row justify-between px-4 bg-secondaryDarkGreyHex mx-4 rounded-xl mt-4"
+          >
+            <TextInput
+              className="bg-[#fff0] flex-1 -mr-6 rounded-xl px-4 text-white"
+              placeholder="Enter promo code"
+              multiline={false}
+              onFocus={() => {
+                console.log("focus");
+              }}
+              placeholderTextColor={"gray"}
+            />
+            <TouchableOpacity
+              className="bg-white rounded-full p-2"
+              activeOpacity={0.7}
+            >
+              <Icons.ArrowRightIcon color="black" size={30} />
+            </TouchableOpacity>
+          </LinearGradient>
+        </BottomSheetView>
+      </BottomSheet>
     </View>
   );
 };
