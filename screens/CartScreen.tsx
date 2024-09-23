8ec0a6a4403518e737765c6ac2bd39d62d4fa948
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-23 11:07:53 +0300

    initialize the payment screen

--- screens/CartScreen.tsx
+++ screens/CartScreen.tsx
@@ -24,6 +24,8 @@ import PersonalPromoCodeCard from "@/components/PersonalPromoCodeCard";
 import { selectCarts } from "@/features/cartSlice";
 import { useSelector } from "react-redux";
 
+import { StripeProvider } from "@stripe/stripe-react-native";
+
 const CartScreen = ({ navigation }: { navigation: any }) => {
   const [isPromoCodeBottomSheetShown, setIsPromoCodeBottomSheetShown] =
     useState(false);
@@ -46,30 +48,47 @@ const CartScreen = ({ navigation }: { navigation: any }) => {
     setPrice(totalPrice);
   }, [carts]); // The effect depends on changes to `carts`
   // console.log(price, "carts");
+
+  const [publishableKey, setPublishableKey] = useState("");
+
+  const fetchPublishableKey = async () => {
+    // const key = await fetchKey(); // fetch key from your server here
+    // setPublishableKey(key);
+  };
+
+  useEffect(() => {
+    fetchPublishableKey();
+  }, []);
+
   return (
-    <View className="flex-1  bg-mainBackground pt-4 ">
-      <Header title="Cart" />
-      <View className="flex-1 ">
-        <FlatList
-          data={carts}
-          renderItem={({ item }) => (
-            <View className="px-4 pt-4">
-              <CartProductCard cart={item} navigation={navigation} />
-            </View>
-          )}
+    <StripeProvider
+      publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY}
+      merchantIdentifier="merchant.identifier" // required for Apple Pay
+      urlScheme="your-url-scheme" // required for 3D Secure and bank redirects
+    >
+      <View className="flex-1  bg-mainBackground pt-4 ">
+        <Header title="Cart" />
+        <View className="flex-1 ">
+          <FlatList
+            data={carts}
+            renderItem={({ item }) => (
+              <View className="px-4 pt-4">
+                <CartProductCard cart={item} navigation={navigation} />
+              </View>
+            )}
+          />
+        </View>
+        <PromoCodeComponent
+          onFocus={() => {
+            bottomSheetRef.current?.expand();
+          }}
+          onPress={() => {}}
+          onTextChange={(text) => {
+            setPromoCode(text);
+          }}
+          textValue={promoCode}
         />
-      </View>
-      <PromoCodeComponent
-        onFocus={() => {
-          bottomSheetRef.current?.expand();
-        }}
-        onPress={() => {}}
-        onTextChange={(text) => {
-          setPromoCode(text);
-        }}
-        textValue={promoCode}
-      />
-      {/* <LinearGradient
+        {/* <LinearGradient
         start={{ x: 0, y: 0 }}
         end={{ x: 1, y: 1 }}
         colors={["#252A32", "#0C0F14"]}
@@ -91,66 +110,67 @@ const CartScreen = ({ navigation }: { navigation: any }) => {
         </TouchableOpacity>
       </LinearGradient> */}
 
-      <View className="flex-row  pt-4 justify-between mb-2">
-        <View className="justify-center items-center pr-10  pl-8 ">
-          <View className="flex-1">
-            <Text className="text-white font-bold text-lg text-center ">
-              Price
-            </Text>
-            <Text className="text-white font-bold text-xl text-center">
-              <Text className="text-primary text-xl">EGP </Text>
-              {price}
-            </Text>
+        <View className="flex-row  pt-4 justify-between mb-2">
+          <View className="justify-center items-center pr-10  pl-8 ">
+            <View className="flex-1">
+              <Text className="text-white font-bold text-lg text-center ">
+                Price
+              </Text>
+              <Text className="text-white font-bold text-xl text-center">
+                <Text className="text-primary text-xl">EGP </Text>
+                {price}
+              </Text>
+            </View>
           </View>
+          <CustomButton
+            title="Ceck Out"
+            handlePress={() => {}}
+            textStyles="text-white text-lg text-bold"
+            containerStyles="bg-primary flex-1 mr-6"
+          />
         </View>
-        <CustomButton
-          title="Ceck Out"
-          handlePress={() => {}}
-          textStyles="text-white text-lg text-bold"
-          containerStyles="bg-primary flex-1 mr-6"
-        />
-      </View>
-      <BottomSheet
-        ref={bottomSheetRef}
-        snapPoints={[450, 700]}
-        index={-1}
-        backgroundStyle={{ backgroundColor: "#2B2D31" }}
-        handleIndicatorStyle={{ backgroundColor: "white" }}
-        handleStyle={{ borderRadius: 30 }}
-        containerHeight={500}
-        enablePanDownToClose
-      >
-        <BottomSheetView>
-          <PromoCodeComponent
-            onFocus={() => {}}
-            onPress={() => {
-              bottomSheetRef.current?.close();
-            }}
-            onTextChange={(text) => {
-              setPromoCode(text);
-            }}
-            textValue={promoCode}
-          />
-          <Text className="text-white text-xl font-semibold px-6 pt-6">
-            Your Promo Codes
-          </Text>
+        <BottomSheet
+          ref={bottomSheetRef}
+          snapPoints={[450, 700]}
+          index={-1}
+          backgroundStyle={{ backgroundColor: "#2B2D31" }}
+          handleIndicatorStyle={{ backgroundColor: "white" }}
+          handleStyle={{ borderRadius: 30 }}
+          containerHeight={500}
+          enablePanDownToClose
+        >
+          <BottomSheetView>
+            <PromoCodeComponent
+              onFocus={() => {}}
+              onPress={() => {
+                bottomSheetRef.current?.close();
+              }}
+              onTextChange={(text) => {
+                setPromoCode(text);
+              }}
+              textValue={promoCode}
+            />
+            <Text className="text-white text-xl font-semibold px-6 pt-6">
+              Your Promo Codes
+            </Text>
 
-          <FlatList
-            data={[1, 2, 3, 4, 45, 6, 7, 8]}
-            keyExtractor={(i) => i.toString()}
-            renderItem={({ item }) => (
-              <PersonalPromoCodeCard
-                onApplyPromoCode={(text: string) => {
-                  setPromoCode(text);
-                }}
-                bottomSheetRef={bottomSheetRef}
-              />
-            )}
-          />
-          {/* <PersonalPromoCodeCard /> */}
-        </BottomSheetView>
-      </BottomSheet>
-    </View>
+            <FlatList
+              data={[1, 2, 3, 4, 45, 6, 7, 8]}
+              keyExtractor={(i) => i.toString()}
+              renderItem={({ item }) => (
+                <PersonalPromoCodeCard
+                  onApplyPromoCode={(text: string) => {
+                    setPromoCode(text);
+                  }}
+                  bottomSheetRef={bottomSheetRef}
+                />
+              )}
+            />
+            {/* <PersonalPromoCodeCard /> */}
+          </BottomSheetView>
+        </BottomSheet>
+      </View>
+    </StripeProvider>
   );
 };
 
