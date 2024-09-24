e127dbe7c750816e789d0fb517cc66a439fde019
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-09-24 08:57:12 +0300

    finish the laoding view

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,10 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import LottieView from "lottie-react-native";
 
 const Loading = () => {
   return (
-    <View>
-      <Text>Loading</Text>
+    <View className="flex-1  bg-mainBackground pt-4 pb-4">
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={"#0C0F14"}
+        hidden={false}
+      />
+      <View className="flex-1 justify-center items-center">
+        <LottieView
+          source={require("../assets/animations/loading.json")}
+          style={{ width: 200, height: 200, flex: 1 }}
+          autoPlay
+          loop
+        />
+      </View>
     </View>
   );
 };
